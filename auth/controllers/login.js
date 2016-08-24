5a73f13a083952cb23b9e3f566fe4ddf0e3c6c4c
Girish Varma Vegesna <varma467@hotmail.com>
2016-08-23 23:58:05 -0700

    Add Basic Login and Signup

--- auth/controllers/login.js
+++ auth/controllers/login.js
@@ -1,3 +1,23 @@
+const User = require('../models/user'); 
+
 module.exports = function(req, res, next) {
-	res.send('login');
+	const email = req.body.email
+	const password = req.body.password
+
+	// Do some more validations (bad json) also check for
+	// code errors showing up when bad req
+	if(email === undefined || password === undefined) {
+		res.send({ error : 'Bad Request'})
+	}
+	User.findOne({ 'email' : email }, function(err, existingUser) {
+		if(err) {
+			return next(err);
+		}
+		if(existingUser) {
+			if(existingUser.password === password) {
+				res.send("loggen in");
+			}
+			res.send("Incorrect email or password");
+		}
+	})
 }
--- auth/controllers/signup.js
+++ auth/controllers/signup.js
@@ -1,5 +1,30 @@
 const User = require('../models/user'); 
 
 module.exports = function(req, res, next) {
-	res.send("singup");
+	const email = req.body.email
+	const password = req.body.password
+
+	// Do some more validations (bad json) also check for
+	// code errors showing up when bad req
+	if(email === undefined || password === undefined) {
+		res.send({ error : 'Bad Request'})
+	}
+	User.findOne({ 'email' : email }, function(err, existingUser) {
+		if(err) {
+			return next(err);
+		}
+		if(existingUser) {
+			return res.status(422).send({ error : 'Email is already used' })
+		}
+		const user = new User({
+			email: email,
+			password: password
+		})
+		user.save(function(err, user) {
+			if(err) {
+				return next(err);
+			}
+			res.json(user);
+		})
+	})
 }
